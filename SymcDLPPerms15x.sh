2e015d5273cfb4ac74acb1a631cc3a11890bd090
chandama <chandler.taylor05@gmail.com>
2019-11-19 13:03:49 -0700

    Added warning message and fixed comment spelling issues

--- SymcDLPPerms15x.sh
+++ SymcDLPPerms15x.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
-# Using file paths.txt to store output of 'find' which prints all perms in octal and rwx format
-# Then running egrep on the paths.txt file to filter via RegEx and filtering output >> permissions.txt
+# Written by: Chandler Taylor
+# Date: November 19, 2019
+#
+# TODO: Add switch to keep output files as 'log' files, if not, discard 'permissions.txt' and 'non_std_perms.txt'
+
+
+# Having write permissions anywhere except the ownership octal is a security risk
+# Using file 'paths.txt' to store output of 'find' which prints all perms in octal and -rwx- format
+# Then running 'egrep' on the 'paths.txt' to filter via RegEx and filtering output >> 'permissions.txt'
 print_write_func(){
+
+        find "$1" -printf '%m\t%M\t%u\t%g\t%p\n' >> $HOME/paths.txt
         echo "-----------------------------------------------------------------" > $HOME/permissions.txt
         echo "#                    Write Perms in Usr/Grp                      #" >> $HOME/permissions.txt
         echo "-----------------------------------------------------------------" >> $HOME/permissions.txt
         echo -e 'Oct\tPerm\t\tUser\tGrp\t\tPath' >> $HOME/permissions.txt
         echo -e '---\t----\t\t----\t---\t\t----' >> $HOME/permissions.txt
-        find "$1" -printf '%m\t%M\t%u\t%g\t%p\n' >> $HOME/paths.txt
         #Look for write permissions in the 2nd or 3rd digit of the Octal -rwx- perms
         egrep '^[0-7][2367][0-7]|^[0-7][0-7][2367]' $HOME/paths.txt >> $HOME/permissions.txt
+
 }
 
-# On standard RHEL DLP install we have 3 perms:
+# On standard RHEL DLP install we have 4 perms:
 #       1. Directories are 755
-#       2. Generic Files are 750
-#       3. Encryption and SSLKeystores are 644
-# This function will locate and display all non 755,750, and 644 perms
+#       2. Generic files are 750
+#       3. Encryption files and SSLKeystore files are 644
+#       4. Log files are 664
+# This function will locate and display all non 755, 750, 664, and 644 perms
 print_non_std_perms(){
+
+        find "$1" -printf '%m\t%M\t%u\t%g\t%p\n' >> $HOME/paths.txt
         echo "-----------------------------------------------------------------" > $HOME/non_std_perms.txt
         echo "#                     Non Std Permissions                       #" >> $HOME/non_std_perms.txt
         echo "-----------------------------------------------------------------" >> $HOME/non_std_perms.txt
         grep -v '664\|644\|755\|750' $HOME/paths.txt >> $HOME/non_std_perms.txt
+
 }
 
+# Take 
 modify_perms(){
+
         declare -a permArray
         declare -i ELEMENTS
 
@@ -33,9 +48,10 @@ modify_perms(){
         ELEMENTS="${#permArray[@]}"
 
         for ((i=0;i<ELEMENTS;i=i+5)) {
-                echo "Element [$i]: ${permArray[$i]} : ${permArray[$i+1]} : ${permArray[$i+4]}"
-                #Check first letter of extended rwx perms to determine if file is symlink, dir, or file
-                #Directories
+                #echo "Element [$i]: ${permArray[$i]} : ${permArray[$i+1]} : ${permArray[$i+4]}"
+                #Check first letter of extended -rwxrwxrwx perms to determine if file is symlink, dir, or file
+                #       d = directory
+                #       - = standard file
                 MOD_PATH=${permArray[$i+4]}
                 if [[ ${permArray[$i+4]} == *"15.1"* ]]; then
                         MOD_PATH=${MOD_PATH/EnforceServer/Enforce Server}
@@ -43,7 +59,7 @@ modify_perms(){
                 if [[ ${permArray[$i+1]} == d* ]]; then
                         echo "Modifying directory: $MOD_PATH"
                         chmod 755 "$MOD_PATH"
-                #Log files use 664 perms
+                #Log files use 664 perms (RegEx captures files ending in .log, .lck, .log.n and .log.n.lck)
                 elif [[ ${permArray[$i+1]} == -* ]] && [[ ${permArray[$i+4]} =~ .+\.log?[\.]?[\d]?.+ || ${permArray[$i+4]} =~ localhost.*\.txt ]]; then
                         echo "Modifying log: $MOD_PATH"
                         chmod 664 "$MOD_PATH"
@@ -58,25 +74,52 @@ modify_perms(){
                 else
                         echo "Unable to determine file type : $MOD_PATH"
                 fi
+
         }
 }
 
-#Function Calls
-print_write_func "$1"
-print_non_std_perms
-echo -n "Modify non standard perms? [y]es [n]o: "
-read PERM_CALL
+echo "
+#####################################################################################
+#                                                                                   #
+#          This script is only intended for use with Symantec DLP 15.x              #
+#      Do not use this script on any directory other than the SymantecDLP 15.x      #
+#  installation directories or else system permissions may be altered incorrectly.  #
+#                                                                                   #
+#      THIS SCRIPT IS NOT LICENSED OR ENDORSED BY SYMANTEC USE AT YOUR OWN RISK     #
+#                                                                                   #
+#####################################################################################
 
-if [[ $PERM_CALL == y ]]; then
-        modify_perms
-elif [[ $PERM_CALL != y ]]; then
-        echo "No modifications made"
-else
-        echo "Invalid entry"
-fi
+Please make a selection:
+1: View incorrect write permissions 
+2: View non-standard permissions
+3: Modify non-standard permissions to default"
+read selection
 
-#In the end, sub $HOME for /tmp or something or take a switch value for an output location and send
-#       results to this location.
-#cat $HOME/permissions.txt
-#cat $HOME/non_std_perms.txt
-rm -f $HOME/paths.txt
+case $selection in
+        1)
+                print_write_func "$1"
+                cat $HOME/permissions.txt
+                rm -f $HOME/paths.txt
+                ;;
+        2)
+                print_non_std_perms "$1"
+                cat $HOME/non_std_perms.txt
+                rm -f $HOME/paths.txt
+                ;;
+        3)
+                echo -n "Modify non standard perms? [y]es [n]o: "
+                read prompt
+                if [[ $prompt == y ]]; then
+                        print_write_func "$1"
+                        modify_perms
+                        rm -f $HOME/paths.txt
+                elif [[ $PERM_CALL == n ]]; then
+                        echo "No modifications made"
+                else
+                        echo "Invalid entry"
+                fi
+                ;;
+        *)
+                echo "Invalid entry"
+                ;;
+esac
